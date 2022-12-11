75b9efa4adfbcfe5d53b383b95dec81959357797
Ardiansyah1506 <112840407+Ardiansyah1506@users.noreply.github.com>
2022-12-11 21:54:16 +0700

    Update fungsi.js

--- minggu12/fungsi.js
+++ minggu12/fungsi.js
@@ -7,16 +7,14 @@ var dataBarang = [
 
 function showBarang() {
     var listBarang = document.getElementById("list-barang");
-    // clear list barang 
     listBarang.innerHTML = "";
 
-    // cetak semua barang 
+
     for (let i = 0; i < dataBarang.length; i++) {
         var btnEdit = "<a href='#' onclick='editBarang(" + i + ")'>Edit</a>";
-        var btnHapus = "<a href='#' 
-        onclick = 'deleteBarang(+i+)' > Hapus < /a>"; 
+        var btnHapus = "<a href='#' onclick = 'deleteBarang(" + i + ")' > Hapus </a>";
 
-        listBarang.innerHTML += "<li>" + dataBarang[i] + " [" + btnEdit + " | " + btnHapus + "] < /li>";
+        listBarang.innerHTML += "<li>" + dataBarang[i] + " [" + btnEdit + " | " + btnHapus + "] </li>";
     }
 }
 
